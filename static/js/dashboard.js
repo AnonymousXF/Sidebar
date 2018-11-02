875067283140562d4f31ea2546a6dc5eee6ecd71
AnonymousXF <464059291@qq.com>
2018-11-02 15:25:53 +0800

    Create dashboard.js

---
+++ static/js/dashboard.js
@@ -0,0 +1,14 @@
+$(".close").click(function () {
+    $("#hostname").val("");
+    $("#hostip").val("");
+    $("#hostuser").val("");
+    $("#hostpwd").val("");
+    $("#hostpwd_confirm").val("");
+});
+$(".btn-secondary").click(function () {
+    $("#hostname").val("");
+    $("#hostip").val("");
+    $("#hostuser").val("");
+    $("#hostpwd").val("");
+    $("#hostpwd_confirm").val("");
+});
