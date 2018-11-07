364794e3ac3a5a48fc08bad38a230933dd052006
AnonymousXF <464059291@qq.com>
2018-11-07 14:46:30 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,12 +3,11 @@ import sqlalchemy
 from sqlalchemy.orm import sessionmaker
 
 engine = sqlalchemy.create_engine("mysql+mysqlconnector://root:root@localhost:3306/test")
-Session = sessionmaker(bind=engine)
-db = Session()
+DBSession = sessionmaker(bind=engine)
+
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'ZdTaiWxb5nozs2yf'
 
     from .dashboard import dashboard
     app.register_blueprint(dashboard)
