25eae58028d7c9e19dcdd8be630e47b2146ffbb8
AnonymousXF <464059291@qq.com>
2018-11-07 14:46:07 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,10 +1,11 @@
 # -*- coding:utf-8 -*-
-from flask import render_template, request
+from flask import render_template, request, jsonify
 import os
 import csv
+import json
 from . import dashboard
 from . import info
-from .. import db
+from .. import DBSession
 from ..models import Host
 
 @dashboard.route('/')
@@ -12,26 +13,30 @@ def dashboard_index():
     return render_template("dashboard.html")
 
 @dashboard.route('/appendSingleHost', methods=['POST'])
-def appendSingleHost():
+def AppendSingleHost():
     raw_data = request.get_json()
     # print(raw_data)
+    session = DBSession()
     try:
         if raw_data['hostpwd'] == raw_data['hostpwd_confirm']:
             host = Host(host_name=raw_data['hostname'],
                         host_ip=raw_data['hostip'],
                         user=raw_data['hostuser'],
                         pwd=raw_data['hostpwd'])
-            db.add(host)
-            db.commit()
+            session.add(host)
+            session.commit()
+            session.close()
             return info.alert_success("添加成功！")
         else:
+            session.close()
             return info.alert_warning("前后两次输入密码不一致！")
     except Exception as e:
         print(e)
+        session.close()
         return info.aler_danger("主机信息已存在，添加失败！")
 
 @dashboard.route('/uploadapi', methods=['POST'])
-def upload():
+def Upload():
     try:
         file = request.files['file']
         filename = file.filename
@@ -46,11 +51,12 @@ def upload():
         return "上传失败！Error：" + e.__repr__()
 
 @dashboard.route('/appendHosts', methods=['POST'])
-def appendHosts():
+def AppendHosts():
     raw_data = request.get_json()
     filename = raw_data['filename'].split('\\')[-1]
     base = os.path.abspath(".")
     path = base + "/temporary/"
+    session = DBSession()
     try:
         csv_file = csv.reader(open(path + filename, 'r', encoding='utf-8'))
         for line in csv_file:
@@ -58,9 +64,24 @@ def appendHosts():
                         host_ip=line[1],
                         user=line[2],
                         pwd=line[3])
-            db.add(host)
-        db.commit()
+            session.add(host)
+        session.commit()
+        session.close()
         return info.alert_success("添加成功！")
     except Exception as e:
         print(e)
+        session.close()
         return info.aler_danger("添加失败！请检查主机信息文件！")
+
+@dashboard.route('/queryhosts')
+def QueryHosts():
+    data = []
+    session = DBSession()
+    results = session.query(Host).all()
+    for result in results:
+        r = {}
+        r['host_name'] = result.host_name
+        r['host_ip'] = result.host_ip
+        data.append(r)
+    session.close()
+    return json.dumps(data)
