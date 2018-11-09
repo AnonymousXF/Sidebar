6935b6af68aad020f1ccc6576c37cbc6093b7ef2
AnonymousXF <464059291@qq.com>
2018-11-09 10:51:14 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -10,6 +10,8 @@ def create_app():
     app = Flask(__name__)
 
     from .dashboard import dashboard
+    from .api import api
     app.register_blueprint(dashboard)
+    app.register_blueprint(api, url_prefix='/api')
 
     return app
